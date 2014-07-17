27c0956993ea2aa0c245c8a3f195c8aeee5511ed
Rafael Corrêa Gomes <rafaelcg_stz@hotmail.com>
2014-07-17 00:09:57 -0300

    Option for install curl or wget

--- system/ohmyzsh.sh
+++ system/ohmyzsh.sh
@@ -5,5 +5,21 @@
 #
 
 ohmyzsh.sh(){
-	wget --no-check-certificate http://install.ohmyz.sh -O - | sh
+	clear
+    echo -e "
+${Yellow}Mode for install Wget or Curl ?
+${txtrst}1) Wget
+2) Curl
+
+"
+
+read ohmyzsh
+case $ohmyzsh in
+
+    # Performs the function with the name of the variable passed
+    1) wget --no-check-certificate http://install.ohmyz.sh -O - | sh;;
+    2) sudo apt-get install curl; curl -L http://install.ohmyz.sh | sh;;
+    *) ohmyzsh.sh;;
+
+esac
 }
