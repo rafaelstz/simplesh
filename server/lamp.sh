f0684ca66c8732269545a388e20df7af196a565b
Rafael Corrêa Gomes <rafaelcg_stz@hotmail.com>
2014-08-01 15:19:31 -0300

    clear add

--- server/lamp.sh
+++ server/lamp.sh
@@ -5,11 +5,12 @@
 #
 
 lamp.sh(){
-	sudo apt-get update ;
-	sudo apt-get install -y lamp-server^ ;
-	sudo a2enmod rewrite ;
+	sudo apt-get update;
+	sudo apt-get install -y lamp-server^;
+	sudo a2enmod rewrite;
 	sudo apt-get install php5 php5-curl php5-gd php5-mcrypt php5-mysql -y;
-	sudo service apache2 restart ;
+	sudo service apache2 restart;
+	clear;
  	#sudo chmod -Rv 777 /etc/apache2/apache2.conf;
 	#sudo echo "ServerName localhost" > /etc/apache2/apache2.conf;
 	#sudo chmod -Rv 644 /etc/apache2/apache2.conf;
