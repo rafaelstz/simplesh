4e4008e125b0d0ad39ca45e552222f0df05b8424
Rafael Corrêa Gomes <rafaelcg_stz@hotmail.com>
2014-08-01 15:13:49 -0300

    Add modulos php necessários para Magento

--- server/lamp.sh
+++ server/lamp.sh
@@ -5,9 +5,11 @@
 #
 
 lamp.sh(){
-	sudo apt-get update;
-	sudo apt-get install lamp-server^;
-	sudo service apache2 restart;
+	sudo apt-get update ;
+	sudo apt-get install -y lamp-server^ ;
+	sudo a2enmod rewrite ;
+	sudo apt-get install php5 php5-curl php5-gd php5-mcrypt php5-mysql -y;
+	sudo service apache2 restart ;
  	#sudo chmod -Rv 777 /etc/apache2/apache2.conf;
 	#sudo echo "ServerName localhost" > /etc/apache2/apache2.conf;
 	#sudo chmod -Rv 644 /etc/apache2/apache2.conf;
