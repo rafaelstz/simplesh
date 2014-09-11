40d7ff2e7c28e4323f2b7da6e2235270d46b6aa0
Rafael Corrêa Gomes <rafaelcg_stz@hotmail.com>
2014-09-11 17:49:25 -0300

    Yes auto on install Spotify

--- desktop/spotify.sh
+++ desktop/spotify.sh
@@ -7,5 +7,5 @@
 spotify.sh(){	
 	sudo sh -c 'echo "deb http://repository.spotify.com stable non-free" >> /etc/apt/sources.list.d/spotify.list';
 	sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 94558F59;
-	sudo apt-get update && sudo apt-get install spotify-client;
+	sudo apt-get update && sudo apt-get install -y spotify-client;
 }
