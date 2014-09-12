9431e42ffad722d1e18363e37dfc00e2efc7531f
Rafael Corrêa Gomes <rafaelcg_stz@hotmail.com>
2014-09-12 14:57:59 -0300

    PHPMyAdmin add LampServer install

--- server/lamp.sh
+++ server/lamp.sh
@@ -8,7 +8,10 @@ lamp.sh(){
 	sudo apt-get update;
 	sudo apt-get install -y lamp-server^;
 	sudo a2enmod rewrite;
-	sudo apt-get install php5 php5-curl php5-gd php5-mcrypt php5-mysql -y;
+	sudo apt-get install -y php5 php5-curl php5-gd php5-mcrypt php5-mysql;
+	sudo service apache2 restart;
+	sudo apt-get install -y phpmyadmin;
+	sudo php5enmod mcrypt;
 	sudo service apache2 restart;
 	clear;
  	#sudo chmod -Rv 777 /etc/apache2/apache2.conf;
