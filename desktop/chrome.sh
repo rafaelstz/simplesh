9b0c489f0e08b2eae7c82546f2c02cb022fdcd97
Rafael Corrêa Gomes <rafaelcg_stz@hotmail.com>
2014-09-17 13:05:22 -0300

    Correct Chrome version

--- desktop/chrome.sh
+++ desktop/chrome.sh
@@ -4,42 +4,45 @@
 # Contact rafaelcgstz@gmail.com
 #
 
+# Index function
 chrome.sh(){
-	clear;
+    clear;
 
-
+    #Show options
 	echo -e "
-		${txtblu}
-What's your version?
+    ${txtblu}
+    What's your version?
 
-${txtrst}
-1) 32 bits 
-2) 64 bits
-	"
-	    read version
+    ${txtrst}
+    1) 32 bits
+    2) 64 bits
+        ";
 
-case $version in
+    # Get value
+    read version
 
-    # Performs the function with the name of the variable passed
-    1) chrome32 ;
-    2) chrome64 ;
-    *) chrome.sh ;
+    # Case
+    case $version in
 
-esac
+        # Performs the function with the name of the variable passed
+        1) chrome32;;
+        2) chrome64;;
+        *) chrome.sh;;
+
+    esac
 }
 
-#Functions
+    #Functions
+    chrome32(){
+        sudo apt-get install -y libxss1 libappindicator1 libindicator7;
+        wget https://dl.google.com/linux/direct/google-chrome-stable_current_i386.deb;
+        sudo dpkg -i google-chrome*.deb;
+        google-chrome;
+    }
 
-chrome32(){	
-	sudo apt-get install -y libxss1 libappindicator1 libindicator7 ;
-	wget https://dl.google.com/linux/direct/google-chrome-stable_current_i386.deb ;
-	sudo dpkg -i google-chrome*.deb ;
-	google-chrome ;;
-}
-
-chrome64(){
-	sudo apt-get install -y libxss1 libappindicator1 libindicator7 ;
-	wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb ;
-	sudo dpkg -i google-chrome*.deb ;
-	google-chrome ;;
-}
+    chrome64(){
+        sudo apt-get install -y libxss1 libappindicator1 libindicator7;
+        wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb;
+        sudo dpkg -i google-chrome*.deb;
+        google-chrome;
+    }
