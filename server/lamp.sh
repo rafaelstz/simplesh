037a9149561032a9d34de487fd09e3ab2fdadde5
Rafael Corrêa Gomes <rafaelcg_stz@hotmail.com>
2014-09-18 11:16:26 -0300

    PhpMyAdmin in Lamp install

--- server/lamp.sh
+++ server/lamp.sh
@@ -13,7 +13,17 @@ lamp.sh(){
 	sudo apt-get install -y phpmyadmin;
 	sudo php5enmod mcrypt;
 	sudo service apache2 restart;
+	sudo chown root:root /etc/phpmyadmin/ -R;
+	sudo chown root:www-data /etc/phpmyadmin/config-db.php;
+	sudo chown root:www-data /etc/phpmyadmin/htpasswd.setup;
+	sudo ln -s /etc/phpmyadmin/apache.conf /etc/apache2/conf.d/phpmyadmin.conf;
+	sudo /etc/init.d/apache2 reload;
+	sudo ln -s /etc/phpmyadmin/apache.conf /etc/apache2/conf-enabled/phpmyadmin.conf;
+	sudo /etc/init.d/apache2 reload;
+	sudo service apache2 restart;
 	clear;
+	#Reconfigura o PhpMyAdmin
+	# sudo dpkg-reconfigure -plow phpmyadmin
  	#sudo chmod -Rv 777 /etc/apache2/apache2.conf;
 	#sudo echo "ServerName localhost" > /etc/apache2/apache2.conf;
 	#sudo chmod -Rv 644 /etc/apache2/apache2.conf;
