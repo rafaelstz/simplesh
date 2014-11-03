b70bf4adf29f50cd4ee4cf21b76dc9203d0b7009
Rafael Corrêa Gomes <rafaelcg_stz@hotmail.com>
2014-11-03 11:52:31 -0200

    Update before install key

--- desktop/spotify.sh
+++ desktop/spotify.sh
@@ -5,6 +5,7 @@
 #
 
 spotify.sh(){	
+	sudo apt-get update;
 	sudo sh -c 'echo "deb http://repository.spotify.com stable non-free" >> /etc/apt/sources.list.d/spotify.list';
 	sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 94558F59;
 	sudo apt-get update && sudo apt-get install -y spotify-client;
