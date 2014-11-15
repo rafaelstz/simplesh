f8328efc221e3ee2406b57e36280e5601bb14da5
Rafael Corrêa Gomes <rafaelcg_stz@hotmail.com>
2014-11-15 00:24:02 -0200

    Confirmação

--- desktop/gimp.sh
+++ desktop/gimp.sh
@@ -6,7 +6,7 @@
 
 gimp.sh(){
 	sudo apt-get update;
-	sudo apt-get install gimp;
+	sudo apt-get install -y gimp;
 	sudo mv ~/.gimp-2.8 ~/.gimp-2.8.old;
     	cp desktop/files/gimpps.zip ~/; cd ~/; unzip gimpps.zip;
     	clear;
