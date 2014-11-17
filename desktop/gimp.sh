4dafd160eb14911f908b9799abc7bc6b359047e0
Rafael Correa Gomes <rafaelcg_stz@hotmail.com>
2014-11-17 15:15:57 -0200

    Identação

--- desktop/gimp.sh
+++ desktop/gimp.sh
@@ -8,7 +8,7 @@ gimp.sh(){
 	sudo apt-get update;
 	sudo apt-get install -y gimp;
 	sudo mv ~/.gimp-2.8 ~/.gimp-2.8.old;
-    	cp desktop/files/gimpps.zip ~/; cd ~/; unzip gimpps.zip;
-    	clear;
+    cp desktop/files/gimpps.zip ~/; cd ~/; unzip gimpps.zip;
+    clear;
 	gimp;
 }
