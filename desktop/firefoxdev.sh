dfab977cee3e5475ea9863a9953ef59dff5128bd
Rafael Corrêa Gomes <rafaelcg_stz@hotmail.com>
2015-05-29 13:44:47 -0300

    Create firefoxdev.sh

---
+++ desktop/firefoxdev.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# Developed by Rafael Corrêa Gomes
+# Contact rafaelcgstz@gmail.com
+#
+
+firefoxdev.sh(){	
+    sudo add-apt-repository ppa:ubuntu-mozilla-daily/firefox-aurora &&
+    sudo apt-get update &&
+    sudo apt-get install firefox
+}
