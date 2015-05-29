6e2b567801c61281466a5d765da573cedaed25a7
Rafael Corrêa Gomes <rafaelcg_stz@hotmail.com>
2015-05-29 10:26:54 -0300

    Update spotify.sh

--- desktop/spotify.sh
+++ desktop/spotify.sh
@@ -5,8 +5,8 @@
 #
 
 spotify.sh(){	
-	sudo apt-get update;
-	sudo sh -c 'echo "deb http://repository.spotify.com stable non-free" >> /etc/apt/sources.list.d/spotify.list';
-	sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 94558F59;
-	sudo apt-get update && sudo apt-get install -y spotify-client;
+	sudo apt-add-repository -y "deb http://repository.spotify.com stable non-free" &&
+	sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 94558F59 &&
+	sudo apt-get update -qq &&
+	sudo apt-get install -y spotify-client
 }
