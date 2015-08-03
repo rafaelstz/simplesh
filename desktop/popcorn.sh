88890aee850925f8bf8acb2e8680e0884793a17f
Rafael Corrêa Gomes <rafaelcg_stz@hotmail.com>
2015-08-03 15:24:28 -0300

    Before remove app repository

--- desktop/popcorn.sh
+++ desktop/popcorn.sh
@@ -5,6 +5,7 @@
 #
 
 popcorn.sh(){
+	sudo add-apt-repository --remove ppa:webupd8team/popcorntime;
 	sudo add-apt-repository ppa:webupd8team/popcorntime;
 	sudo apt-get update;
 	sudo apt-get install popcorn-time;
