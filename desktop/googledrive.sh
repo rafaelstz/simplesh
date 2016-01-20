493eaca5f9666beefba7ec4b31d50960bb0edd55
Matheus Lima <contato@matheuslima.com.br>
2016-01-20 13:38:48 -0300

    fix the googledrive.sh dependencies

--- desktop/googledrive.sh
+++ desktop/googledrive.sh
@@ -5,7 +5,10 @@
 #
 
 googledrive.sh(){
+
+	sudo add-apt-repository -y ppa:nilarimogard/webupd8 &&
 	sudo add-apt-repository -y ppa:thefanclub/grive-tools &&
 	sudo apt-get update &&
+	sudo apt-get install grive &&
 	sudo apt-get install -y grive-tools
 }
